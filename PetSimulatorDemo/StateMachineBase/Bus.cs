e6f949fecb390936778f51a7df1ffbad631db27e
SunYan <garrysun9993@gmail.com>
2020-03-31 17:43:49 +0800

    Add new lib add rx lib

--- PetSimulatorDemo/StateMachineBase/Bus.cs
+++ PetSimulatorDemo/StateMachineBase/Bus.cs
@@ -1,7 +1,16 @@
+using System.Reactive;
+using System.Reactive.Subjects;
+
 namespace PetSimulatorDemo.StateMachineBase
 {
     public class Bus : IIo
     {
+        public Subject<IMessage> Subject { get; set; }
+
+        public Bus()
+        {
+            Subject = new Subject<IMessage>();
+        }
         public void Push(IMessage message)
         {
             throw new System.NotImplementedException();
